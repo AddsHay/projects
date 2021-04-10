374b2a0f5ae800115bde4ae6f542f8dfd4ddd516
Adrian <adhay88@berkeley.edu>
2021-04-10 13:45:08 -0500

    jhfvbjhdsv

--- proj3/byow/Core/Main.java
+++ proj3/byow/Core/Main.java
@@ -1,10 +1,8 @@
 package byow.Core;
 
 /** This is the main entry point for the program. This class simply parses
- *  the command line inputs, and lets the byow.Core.Engine class take over Derelict!
- *  in either keyboard or input string mode. ewifvbkdhbabvdhwakbkh
- *
- *  bvhbvdkhvdnbkhv
+ *  the command line inputs, and lets the byow.Core.Engine class take over
+ *  in either keyboard or input string mode.
  */
 public class Main {
     public static void main(String[] args) {
