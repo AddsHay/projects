9393b01f356c9d45ca5eb10569115d0467f44f9d
Adrian <adhay88@berkeley.edu>
2021-04-17 17:28:10 -0500

    for gs

--- proj3/byow/Core/Main.java
+++ proj3/byow/Core/Main.java
@@ -107,100 +107,6 @@ public class Main {
             }
         }
 
-        public static void cap(TETile[][] tiles) {
-            int height = tiles[0].length;
-            int width = tiles.length;
-            for (int x = 0; x < width; x += 1) {
-                for (int y = 0; y < height; y += 1) {
-                    if (tiles[x][y] == Tileset.FLOOR) {
-                        if (tiles[x + 1][y] == Tileset.NOTHING || tiles[x][y - 1] == Tileset.NOTHING
-                                || tiles[x - 1][y] == Tileset.NOTHING || tiles[x][y + 1] == Tileset.NOTHING
-                                || tiles[x + 1][y - 1] == Tileset.NOTHING || tiles[x - 1][y - 1] == Tileset.NOTHING
-                                || tiles[x - 1][y + 1] == Tileset.NOTHING || tiles[x + 1][y + 1] == Tileset.NOTHING) {
-                            tiles[x][y] = Tileset.WALL;
-                        }
-                    }
-                }
-            }
-        }
-
-        public static void randomexit(TETile[][] tiles, Pos p, int dx, int dy) {
-            int check = RandomUtils.uniform(RANDOM, -Math.abs(dx) - Math.abs(dy) + 4, Math.abs(dx) + Math.abs(dy) - 4);
-            if (check < -dx + 2) {
-                if (p.x - 3 > 0) {
-                    p.y += check + dy - 2;
-                } else {
-                    check = 0;
-                }
-            } else if (check < 0) {
-                if (p.y - 3 > 0) {
-                    p.x += check;
-                } else {
-                    check = 0;
-                }
-            } else if (check > dx - 2) {
-                if (p.x + dx + 3 < WIDTH) {
-                    p.y += check - dy + 2;
-                    p.x += dx - 1;
-                } else {
-                    check = 0;
-                }
-            } else if (check > 0) {
-                if (p.y + dy + 3 < HEIGHT) {
-                    p.x += check;
-                    p.y += dy - 1;
-                } else {
-                    check = 0;
-                }
-            }
-            if (check == 0) {
-                randomexit(tiles, p, dx, dy);
-            } else {
-                tiles[p.x][p.y] = Tileset.FLOOR;
-            }
-        }
-
-        public static void randombuilder(TETile[][] tiles, TETile walltile, TETile floortile, Pos p, int dx, int dy) {
-            int type = 0;
-            if (tiles[p.x - 1][p.y] == Tileset.WALL && tiles[p.x + 1][p.y] == Tileset.WALL) {
-                if (tiles[p.x][p.y - 1] == Tileset.FLOOR) {
-                    // Vertically up
-                    type = 1;
-                } else if (tiles[p.x][p.y + 1] == Tileset.FLOOR) {
-                    // Vertically down
-                    type = -1;
-                }
-            } else if (tiles[p.x][p.y - 1] == Tileset.WALL && tiles[p.x][p.y + 1] == Tileset.WALL) {
-                if (tiles[p.x - 1][p.y] == Tileset.FLOOR) {
-                    // Horizontally right
-                    type = 2;
-                } else if (tiles[p.x + 1][p.y] == Tileset.FLOOR) {
-                    // Horizontally left
-                    type = -2;
-                }
-            }
-            int check = RandomUtils.uniform(RANDOM, 0, 2);
-            if (check == 0) {
-                // room
-                if (Math.abs(type) == 1) {
-                    // upward/downward room
-                    createroom(tiles, walltile, floortile, p, dx * type, dy * type);
-                } else if (Math.abs(type) == 2) {
-                    // rightward/leftward room
-                    createroom(tiles, walltile, floortile, p, dx * type / 2, dy * type / 2);
-                }
-            } else {
-                // hall
-                if (Math.abs(type) == 1) {
-                    // upward/downward hall
-                    createhallvert(tiles, walltile, floortile, p, dy * type);
-                } else if (Math.abs(type) == 2) {
-                    // rightward/leftward hall
-                    createhallhor(tiles, walltile, floortile, p, dx * type / 2);
-                }
-            }
-        }
-
         public static void fillWithNothing(TETile[][] tiles) {
             int height = tiles[0].length;
             int width = tiles.length;
@@ -220,21 +126,6 @@ public class Main {
                 return p;
             } else {
                 return d;
-            }
-        }
-
-        public static void drawworld(TETile[][] tiles, TETile walltile, TETile floortile) {
-            fillWithNothing(tiles);
-            int opcount = RandomUtils.uniform(RANDOM, 10, 50);
-            Pos p = new Pos(RandomUtils.uniform(RANDOM, 10, WIDTH - 10), RandomUtils.uniform(RANDOM, 10, HEIGHT - 10));
-            int dx = createdimension(p.x, WIDTH);
-            int dy = createdimension(p.y, HEIGHT);
-            createroom(tiles, walltile, floortile, p, dx, dy);
-            for (int z = 0; z < opcount; z++) {
-                randomexit(tiles, p, dx, dy);
-                dx = createdimension(p.x, WIDTH);
-                dy = createdimension(p.y, HEIGHT);
-                randombuilder(tiles, walltile, floortile, p, dx, dy);
             }
         }
 
